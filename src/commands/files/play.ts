d00a1441d65c04229e67e90d9d5628e35fd75688
Jan Pieczul <jannos1999@gmail.com>
2022-01-10 00:28:30 +0100

    implement queue v1

--- src/commands/files/play.ts
+++ src/commands/files/play.ts
@@ -1,65 +1,34 @@
-import {
-  AudioPlayer,
-  createAudioPlayer, createAudioResource,
-  getVoiceConnection,
-  NoSubscriberBehavior
-} from "@discordjs/voice";
-
 import { MyCommand, MyCommandProps } from "interfaces/MyCommand";
 import { joinVoice } from "../../services/joinVoice";
 import { isValidURL } from "../../utils";
-import { downloadSong } from "../../lib/youtubeDownloader";
 import { GuildStorage } from "../../lib/guildStorage";
-
-const findOrCreatePlayer = (guildId: string): AudioPlayer => {
-  const connection = getVoiceConnection(guildId)
-
-  if (!connection) {
-    throw 'Connection not found'
-  }
-
-  const guildExtension = GuildStorage.getGuildExtension(guildId);
-  connection.on('stateChange', (oldState, newState) => {
-    if (newState.status === 'disconnected' || newState.status === 'destroyed') {
-      delete guildExtension.player
-    }
-  })
-
-  if (!guildExtension.player) {
-    const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause, }, });
-    connection?.subscribe(player)
-    guildExtension.player = player
-  }
-
-  return guildExtension.player
-}
-
+import { createAudioResource } from "@discordjs/voice";
+import { downloadSong } from "../../lib/youtubeDownloader";
 
 export default class implements MyCommand {
   about = "Plays youtube link";
 
   async handleMessage({ message, args }: MyCommandProps) {
-    // const { channel: { send } } = message
+    const { channel: { send } } = message
 
-    if (!message.guildId) {
+    if (!message.guildId || message.channel.type !== 'GUILD_TEXT') {
       return
     }
 
     if (args.length < 1) {
-      return message.channel.send('Wrong number of args')
+      return send('Wrong number of args')
     }
 
     if (!isValidURL(args[0])) {
-      return message.channel.send('Not a valid URL')
+      return send('Not a valid URL')
     }// TODO else get URL from search
 
     await joinVoice(message)
-    const player = findOrCreatePlayer(message.guildId)
-
-    message.channel.send('Downloading')
-    downloadSong(args[0]).then((outputPath) => {
-      message.channel.send('Playing')
-      player.play(createAudioResource(outputPath))
-    })
+    try {
+      await GuildStorage.getGuildExtension(message.guildId).queue.add(args[0])
+      message.channel.send('Enqueued')
+    } catch (e: any) {
+      message.channel.send(e.toString())
+    }
   }
 }
--- src/lib/guildStorage/index.ts
+++ src/lib/guildStorage/index.ts
@@ -1,15 +1,100 @@
-import { AudioPlayer } from "@discordjs/voice";
+import {
+  AudioPlayer,
+  AudioPlayerStatus,
+  createAudioPlayer,
+  createAudioResource,
+  getVoiceConnection,
+  NoSubscriberBehavior
+} from "@discordjs/voice";
+import { downloadSong } from "../youtubeDownloader";
 
-type GuildExtension = {
-  player: AudioPlayer
+class MyQueue {
+  songs: string[] = []
+
+  constructor(private parent: GuildExtension) {
+  }
+
+  async add(url: string) {
+    try {
+      const outputPath = await downloadSong(url)
+      this.songs.push(outputPath)
+      if (!this.isPlaying()) {
+        this.playNext()
+      }
+    } catch (e) {
+      return e
+    }
+  }
+
+  playNext() {
+    const player = this.player
+    if (player) {
+      const nextSong = this.songs.shift()
+      if (nextSong) {
+        player.play(createAudioResource(nextSong))
+      }
+    }
+  }
+
+  private isPlaying() {
+    return this.player?.state.status === AudioPlayerStatus.Playing;
+  }
+
+  private get player() {
+    return this.parent.getPlayer()
+  }
+}
+
+class GuildExtension {
+  guildId: string
+  player?: AudioPlayer
+  queue: MyQueue
+
+  get connection() {
+    return getVoiceConnection(this.guildId)
+  }
+
+  constructor(guildId: string) {
+    this.guildId = guildId;
+    this.queue = new MyQueue(this)
+  }
+
+  getPlayer(): AudioPlayer | undefined {
+    if (this.player) {
+      return this.player
+    }
+
+    this.createSubscribedPlayer()
+
+    return this.player
+  }
+
+  private createSubscribedPlayer() {
+    const connection = this.connection
+
+    if (!connection) {
+      throw `Client is not in any voice channel belonging to the guild#${this.guildId}`
+    }
+
+    connection.on('stateChange', (oldState, newState) => {
+      if (newState.status === 'disconnected' || newState.status === 'destroyed') {
+        delete this.player
+      }
+    })
+
+    const player = createAudioPlayer({ behaviors: { noSubscriber: NoSubscriberBehavior.Pause, }, });
+    connection?.subscribe(player)
+    player.on(AudioPlayerStatus.Idle, () => this.queue.playNext())
+    this.player = player
+  }
 }
 
 export class GuildStorage {
-  static guilds: Record<string, Partial<GuildExtension>> = {}
+  static guilds: Record<string, GuildExtension> = {}
 
   static getGuildExtension(guildId: string) {
     if (!(guildId in this.guilds)) {
-      this.guilds[guildId] = {}
+      this.guilds[guildId] = new GuildExtension(guildId)
     }
 
     return this.guilds[guildId]
